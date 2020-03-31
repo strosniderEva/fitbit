f89c76d2591d23aa6b2c0afc605999a2cf542952
JDDean97 <62315818+JDDean97@users.noreply.github.com>
2020-03-30 22:43:40 -0400

    Responsive Layout
    Added style sheet used by all models except Ionic

---
+++ Practice-master/app/index.js
@@ -0,0 +1,233 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+
+import { me as appbit } from "appbit";
+import { me as device } from "device";
+import { today} from "user-activity";
+import { goals } from "user-activity";
+
+if (appbit.permissions.granted("access_activity")) {
+//    console.log(`${today.adjusted.steps} Steps`);
+//    console.log(`${today.adjusted.elevationGain} Floor(s)`);
+//    console.log(`${today.adjusted.calories} Calories(s)`);
+//    console.log(`${today.adjusted.distance} Distance`);
+
+  
+//    if (goals.steps !== undefined) {
+//      console.log(`${goals.steps} Steps Goal`);
+//    }
+//   if (goals.calories !== undefined) {
+//      console.log(`${goals.calories} Calories Goal`);
+//    }
+//   if (goals.distance !== undefined) {
+//      console.log(`${goals.distance} Distance Goal`);
+//   }
+
+//    if (goals.elevationGain !== undefined) {
+//      console.log(`${goals.elevationGain} Floor Goal`);
+//    }
+  
+//   if (goals.calories !== undefined) {
+//      console.log(`${goals.calories} Calories Goal`);
+//    }
+  
+//   if (goals.distance !== undefined) {
+//      console.log(`${goals.distance} Distance Goal`);
+//    }
+  
+}
+
+// Update the clock every second
+clock.granularity = "seconds";
+
+// Get a handle on the <text> element
+const myClock = document.getElementById("myClock");
+const txtSteps = document.getElementById("txtSteps");
+const txtElev = document.getElementById("txtElev");
+const txtDist = document.getElementById("txtDist");
+const txtCal = document.getElementById("txtCal");
+
+
+  
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  
+  //Update step counter 
+  let now = evt.date;
+  let hours = now.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(now.getMinutes());
+  myClock.text = `${hours}:${mins}`;
+  
+   let stepCount = 0;
+   let elevation = 0;
+   let distance = 0;
+   let calories = 0;
+   let stepCountGoal = 0;
+   let elevationGoal = 0;
+   let distanceGoal = 0;
+   let caloriesGoal = 0;
+  
+  if (appbit.permissions.granted("access_activity")) {
+
+   stepCount = today.adjusted.steps || 0;
+   elevation = today.adjusted.elevationGain || 0;
+   distance = today.adjusted.distance || 0;
+   calories = today.adjusted.calories || 0;
+    
+   stepCountGoal = goals.steps;
+   elevationGoal = goals.elevationGain;
+   distanceGoal = goals.distance;
+   caloriesGoal = goals.calories;
+   
+    txtSteps.text = stepCount.toString();
+    txtElev.text = elevation.toString();
+    txtDist.text = distance.toString() + 'm';
+    txtCal.text = calories.toString();
+
+  } 
+  
+  
+    
+  //COLORS!
+  //Time  
+  var time = now.getHours();
+  switch(true){
+       case (time <= 2):
+      document.getElementById("hbgOne").style.fill = "#1e122a"; 
+        break;
+       case (time <= 4):
+
+      document.getElementById("hbgOne").style.fill = "#5c264a"; 
+        break;
+       case (time <= 6):
+      document.getElementById("hbgOne").style.fill = "#a23b56"; 
+        break;
+       case (time <= 8):
+        document.getElementById("hbgOne").style.fill = "#de614d"; 
+        break;
+       case (time <= 10):
+        document.getElementById("hbgOne").style.fill = "#fe9b36"; 
+        break;
+       case (time <= 14):
+        document.getElementById("hbgOne").style.fill = "#ffdf21"; 
+        break;
+       case (time <= 16):
+        document.getElementById("hbgOne").style.fill = "#fe9b36"; 
+        break;
+       case (time <= 18):
+        document.getElementById("hbgOne").style.fill = "#de614d"; 
+        break;
+       case (time <= 20):
+        document.getElementById("hbgOne").style.fill = "#a23b56"; 
+        break;
+       case (time <= 22):
+        document.getElementById("hbgOne").style.fill = "#5c264a"; 
+        break;
+       case (time <= 24):
+        document.getElementById("hbgOne").style.fill = "#1e122a"; 
+        break;
+         }
+  
+  //Steps
+  
+  switch(true){
+      case (stepCount <= (stepCountGoal*.19)):
+        document.getElementById("hbgTwo").style.fill = "#61a722"; 
+        break;
+      case (stepCount <= (stepCountGoal*.39)):
+        document.getElementById("hbgTwo").style.fill = "#7fb31c"; 
+        break;
+      case (stepCount <= (stepCountGoal*.59)):
+        document.getElementById("hbgTwo").style.fill = "#9ebf17"; 
+        break;
+         case (stepCount <= (stepCountGoal*.79)):
+        document.getElementById("hbgTwo").style.fill = "#bbcb15"; 
+        break;
+      case (stepCount <= (stepCountGoal*.99)):
+        document.getElementById("hbgTwo").style.fill = "#ded518"; 
+        break;
+      case (stepCount >= (stepCountGoal)):
+        document.getElementById("hbgTwo").style.fill = "#ffdf21"; 
+        break;
+         
+  }
+  
+  //Calories
+  switch(true){
+      case (calories <= (caloriesGoal*.19)):
+        document.getElementById("hbgFive").style.fill = "#c74529"; 
+        break;
+      case (calories <= (caloriesGoal*.39)):
+        document.getElementById("hbgFive").style.fill = "#da6224"; 
+        break;
+      case (calories <= (caloriesGoal*.59)):
+        document.getElementById("hbgFive").style.fill = "#ea801c"; 
+        break;
+         case (calories <= (caloriesGoal*.79)):
+        document.getElementById("hbgFive").style.fill = "#f59f14"; 
+        break;
+      case (calories <= (caloriesGoal*.99)):
+        document.getElementById("hbgFive").style.fill = "#febf13"; 
+        break;
+      case (calories >= (caloriesGoal)):
+        document.getElementById("hbgFive").style.fill = "#ffdf21"; 
+        break;
+         
+  }
+  
+  //Elevation
+  switch(true){
+      case (elevation <= (elevationGoal*.19)):
+        document.getElementById("hbgThree").style.fill = "#1a4e7d"; 
+        break;
+      case (elevation <= (elevationGoal*.39)):
+        document.getElementById("hbgThree").style.fill = "#0074a0"; 
+        break;
+      case (elevation <= (elevationGoal*.59)):
+        document.getElementById("hbgThree").style.fill = "#0099a1"; 
+        break;
+         case (elevation <= (elevationGoal*.79)):
+        document.getElementById("hbgThree").style.fill = "#00ba81"; 
+        break;
+      case (elevation <= (elevationGoal*.99)):
+        document.getElementById("hbgThree").style.fill = "#85d44d"; 
+        break;
+      case (elevation >= (elevationGoal)):
+        document.getElementById("hbgThree").style.fill = "#ffdf21"; 
+        break;
+  }
+  
+//Distance
+  switch(true){
+      case (distance <= (distanceGoal*.19)):
+        document.getElementById("hbgFour").style.fill = "#911f53"; 
+        break;
+      case (distance <= (distanceGoal*.39)):
+        document.getElementById("hbgFour").style.fill = "#bd3750"; 
+        break;
+      case (distance <= (distanceGoal*.59)):
+        document.getElementById("hbgFour").style.fill = "#e05946"; 
+        break;
+         case (distance <= (distanceGoal*.79)):
+        document.getElementById("hbgFour").style.fill = "#f88237"; 
+        break;
+      case (distance <= (distanceGoal*.99)):
+        document.getElementById("hbgFour").style.fill = "#ffaf25"; 
+        break;
+      case (distance >= (distanceGoal)):
+        document.getElementById("hbgFour").style.fill = "#ffdf21"; 
+        break;
+         
+  }
+}
+
